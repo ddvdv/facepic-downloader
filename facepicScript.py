7aa2adee84bd2917c1b96786d110976c6a3fd2e2
David VDV <davidv@Davids-MacBook-Pro.local>
2020-04-05 12:02:05 +0200

    update

--- facepicScript.py
+++ facepicScript.py
@@ -6,10 +6,13 @@ import os
 import requests
 import getpass
 
-email = input("What is your facebook email account ? : ")
-pwd = getpass.getpass(prompt='What is your facebook password ? : ', stream=None)
-url = input("What's the url of the first pic ? : ")
+# email = input("What is your facebook email account ? : ")
+# pwd = getpass.getpass(prompt='What is your facebook password ? : ', stream=None)
+# url = input("What's the url of the first pic ? : ")
 
+email = "clementinecc@gmail.com"
+pwd = "cr1n0l1ne"
+url = "https://www.facebook.com/photo.php?fbid=10162357974705576&set=pb.653320575.-2207520000..&type=3&theater"
 
 # with selenium
 from selenium import webdriver
@@ -28,13 +31,18 @@ browser.find_element_by_css_selector('#email').send_keys(email)
 browser.find_element_by_css_selector('#pass').send_keys(pwd)
 browser.find_element_by_css_selector('.uiButtonConfirm').click()
 
+
 browser.get(url)
+
+
+time.sleep(3)
+
 for i in range(0,100000):
     # get the image source
     imgElem = browser.find_element_by_css_selector('.spotlight')
     src = imgElem.get_attribute('src')
     img = requests.get(src)
-    imageFile = open(os.path.join('pics', os.path.basename("FB_tagged_{}.jpg".format(str(i+1)))), 'wb')
+    imageFile = open(os.path.join('pics_album', os.path.basename("FB_tagged_{}.jpg".format(str(i+1)))), 'wb')
     for chunk in img.iter_content(100000):
         imageFile.write(chunk)
     imageFile.close()
